243ffb3bd548cf9c40223bb110624146c2af8e32
HarshalSrivastava <harshalsrivastava51@gmail.com>
2024-07-09 01:16:44 +0530

    Implemented adding new user to the system + designed screens for sign up or login
    Todo
    1. Implement the login/sign up UI with flow to respective screens
    2. Implement mechanism to verify user credentials while logging in and username uniqueness while signing user up

---
+++ Assets/Scripts/UserLoadSaveManager.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class UserLoadSaveManager : MonoBehaviour
+{
+    [SerializeField]
+    private string saveFileName;
+
+    private string saveFilePath;
+
+    private Dictionary<string, string> availableUserDictionary;
+
+    public Users testData;
+
+    private void Start()
+    {
+        saveFilePath = Path.Combine(Application.dataPath, "Resources", saveFileName + ".json");
+    }
+
+    void LoadAvailableUsersFile()
+    {
+        if (File.Exists(saveFilePath))
+        {
+            string json = File.ReadAllText(saveFilePath);
+            testData = new Users();
+            testData = JsonUtility.FromJson<Users>(json);
+            PopulateAvailableUserDictionary(testData.users);
+            Debug.Log("Data loaded from " + saveFilePath);
+            
+        }
+        else
+        {
+            Debug.LogError("file not found, creating default user");
+            CreateDefaultData();
+            LoadAvailableUsersFile();
+        }
+    }
+
+    void PopulateAvailableUserDictionary(List<UserData> usersFetched)
+    {
+        availableUserDictionary = new Dictionary<string, string>();
+        for (int i=0;i<usersFetched.Count;i++)
+        {
+            availableUserDictionary.Add(usersFetched[i].userName, usersFetched[i].userPassword);
+        }
+    }
+
+    void SaveUserToFile(UserData dataToSave)
+    {
+        Users userList = new Users();
+        // Load existing users from the JSON file
+        if (File.Exists(saveFilePath))
+        {
+            string json = File.ReadAllText(saveFilePath);
+            userList = JsonUtility.FromJson<Users>(json);
+        }
+        else
+        {
+            userList.users = new List<UserData>();
+        }
+
+
+        // Add the new user to the list
+        userList.users.Add(dataToSave);
+
+        // Convert back to JSON
+        string updatedJson = JsonUtility.ToJson(userList);
+
+        // Save the updated JSON to the file
+        File.WriteAllText(saveFilePath, updatedJson);
+        Debug.Log("Saved users to path : " + saveFilePath);
+    }
+
+    void CreateDefaultData()
+    {
+        UserData defaultUserData = new UserData();
+
+        defaultUserData.userName = "DefaultUser";
+        defaultUserData.userPassword = "DefaultPassword";
+
+        SaveUserToFile(defaultUserData);
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            CreateDefaultData();
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            LoadAvailableUsersFile();
+        }
+    }
+}
+
+[System.Serializable]
+public class Users
+{
+    public List<UserData> users;
+}
+
+[System.Serializable]
+public class UserData
+{
+    public string userName;
+    public string userPassword;
+}
+
+
+
